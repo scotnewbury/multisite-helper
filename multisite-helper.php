df62d25e2bbd0563c951b7526530cee2891d1f70
Scot Newbury <scot@scotnewbury.com>
2023-04-27 16:59:01 -0400

    Converted to object oriented code usage

--- multisite-helper.php
+++ multisite-helper.php
@@ -3,7 +3,7 @@
  * Plugin Name: Multisite Helper
  * Plugin URI: https://github.com/scotnewbury/multisite-helper
  * Description: Provides quality of life improvments for administrators of multisite envrionments
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Scot Newbury
  * Author URI: https://scotnewbury.com
  * License: GPL3
@@ -14,29 +14,37 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-/*
- * This function sorts the listing of blogs on the network that the user has access to alphabetically
-*/
+class Multisite_Helper {
+    public function __construct() {
+        add_filter ( 'get_blogs_of_user', array( $this, 'sort_my_multisite_blog_listing' ) );
+        add_action ( 'wp_before_admin_bar_render', array( $this, 'add_new_site_to_multisite' ) );
+    }
 
-function sort_my_multisite_blog_listing ( $blogs ) {
-    uasort( $blogs, function( $a, $b ) { 
-        return strcasecmp( $a->blogname, $b->blogname );
-    });
-    return $blogs;
-};
-add_filter ( 'get_blogs_of_user', 'sort_my_multisite_blog_listing' );
+    /*
+     * This function sorts the listing of blogs on the network that the user has access to alphabetically
+    */
 
-/* This function adds a menu option under Sites to all you to go directly to the 
- * Add New Site page directly without the need to list all the site in the netowrk
- */
+    public function sort_my_multisite_blog_listing ( $blogs ) {
+        uasort( $blogs, function( $a, $b ) { 
+            return strcasecmp( $a->blogname, $b->blogname );
+        });
+        return $blogs;
+    }
 
-function add_new_site_to_multisite() {
-    global $wp_admin_bar;
-    $wp_admin_bar->add_node( array (
-        'parent' => 'network-admin-s',
-        'id' => 'add-new-site',
-        'title' => _( 'Add New Site' ),
-        'href' => network_admin_url( 'site-new.php' ),
-    ));
+    /* This function adds a menu option under Sites to allow you to go directly to the 
+    * Add New Site page without the need to list all the sites in the netowrk
+    */
+
+    function add_new_site_to_multisite() {
+        global $wp_admin_bar;
+        $wp_admin_bar->add_node( array (
+            'parent' => 'network-admin-s',
+            'id' => 'add-new-site',
+            'title' => _( 'Add New Site' ),
+            'href' => network_admin_url( 'site-new.php' ),
+        ));
+    }
 }
-add_action ( 'wp_before_admin_bar_render', 'add_new_site_to_multisite' );
+
+$multisite_helper = new Multisite_Helper();
+
