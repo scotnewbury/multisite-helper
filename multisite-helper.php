7aec172a40cbc956ada6a352fb5faf764d1095e2
Scot Newbury <scot@scotnewbury.com>
2023-04-27 23:29:02 -0400

    Add column headers for blog id

--- multisite-helper.php
+++ multisite-helper.php
@@ -18,6 +18,7 @@ class Multisite_Helper {
     public function __construct() {
         add_filter ( 'get_blogs_of_user', array( $this, 'sort_my_multisite_blog_listing' ) );
         add_action ( 'wp_before_admin_bar_render', array( $this, 'add_new_site_to_multisite' ) );
+        add_filter ( 'wpmu_blogs_columns', array ( $this, 'multisite_blogs_columns' ) );
     }
 
     /*
@@ -44,6 +45,33 @@ class Multisite_Helper {
             'href' => network_admin_url( 'site-new.php' ),
         ));
     }
+
+    /**
+     * This function adds new columns to the site index page in the administration screens.
+     * 
+     * It currently adds the following columns:
+     * Blog ID - The ID used to identify the blog in the multisite environment
+     * 
+     *
+     * @param   array  $sites_columns   The array of columns before additions
+     *
+     * @return  array                    The array after the additional columns are added
+     */
+    function multisite_blogs_columns($sites_columns)
+    {
+
+        $columns_before = array_slice( $sites_columns, 0, 2 );
+        $columns_after = array_slice( $sites_columns, 2 );
+
+        $sites_columns = 
+            $columns_before + 
+            array( 
+                'blog_id' => __( 'Blog ID' ),
+            ) + 
+            $columns_after;
+        return $sites_columns;
+    }
+
 }
 
 $multisite_helper = new Multisite_Helper();
